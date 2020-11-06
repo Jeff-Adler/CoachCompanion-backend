76303277a4a91370626f4d1dbf7d8d8c262f438b
Jeff-Adler <Jeffm.adler@gmail.com>
2020-11-06 13:54:16 -0500

    source

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_many :activities
     has_many :logs
-    # has_many :activities, through: log #need to alias
+    # has_many :activities, through: log #need to alias and input source
     has_secure_password
     validates :username, uniqueness: { case_sensitive: false }
 end
